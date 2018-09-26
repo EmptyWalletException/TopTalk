1144d21af738c73c47f79ad8b4b68b02da753cc3
EmptyWalletException <1833865798@qq.com>
2018-09-26 16:10:15 +0800

    完成了活动模块的搭建; 完成了redis缓存功能的实现;

---
+++ toptalk_gathering/src/main/java/com/toptalk/gathering/GatheringApplication.java
@@ -0,0 +1,21 @@
+package com.toptalk.gathering;
+
+import entity.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+@EnableCaching
+@SpringBootApplication
+public class GatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringApplication.class, args);
+	}
+
+	@Bean
+	public IdWorker idWorkker(){
+		return new IdWorker(1, 1);
+	}
+	
+}
