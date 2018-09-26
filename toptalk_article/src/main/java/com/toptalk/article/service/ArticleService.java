0bd4f5e2e9d103e9aab0a408b2c7351c2a852a62
EmptyWalletException <1833865798@qq.com>
2018-09-26 15:44:08 +0800

    文章模块引入了redis缓存;

--- toptalk_article/src/main/java/com/toptalk/article/service/ArticleService.java
+++ toptalk_article/src/main/java/com/toptalk/article/service/ArticleService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -17,174 +18,197 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 服务层
- * 
- * @author Administrator
  *
+ * @author Administrator
  */
 @Service
 public class ArticleService {
 
-	@Autowired
-	private ArticleDao articleDao;
-	
-	@Autowired
-	private IdWorker idWorker;
+    @Autowired
+    private ArticleDao articleDao;
 
-	/**
-	 * 点赞数+1;
-	 * @param id
-	 * @return
-	 */
-	@Transactional
-	public int updaeThumbup(String id){
-		return articleDao.updateThumbup(id);
-	}
+    @Autowired
+    private IdWorker idWorker;
 
-	/**
-	 * 审核文章通过;
-	 * @param id
-	 */
-	@Transactional
-	public void examine(String id){
-		articleDao.examine(id);
-	}
+    @Autowired
+    private RedisTemplate redisTemplate;
 
-	/**
-	 * 查询全部列表
-	 * @return
-	 */
-	public List<Article> findAll() {
-		return articleDao.findAll();
-	}
+    /**
+     * 点赞数+1;
+     *
+     * @param id
+     * @return
+     */
+    @Transactional
+    public int updaeThumbup(String id) {
+        return articleDao.updateThumbup(id);
+    }
 
-	
-	/**
-	 * 条件查询+分页
-	 * @param whereMap
-	 * @param page
-	 * @param size
-	 * @return
-	 */
-	public Page<Article> findSearch(Map whereMap, int page, int size) {
-		Specification<Article> specification = createSpecification(whereMap);
-		PageRequest pageRequest =  PageRequest.of(page-1, size);
-		return articleDao.findAll(specification, pageRequest);
-	}
+    /**
+     * 审核文章通过;
+     *
+     * @param id
+     */
+    @Transactional
+    public void examine(String id) {
+        articleDao.examine(id);
+    }
 
-	
-	/**
-	 * 条件查询
-	 * @param whereMap
-	 * @return
-	 */
-	public List<Article> findSearch(Map whereMap) {
-		Specification<Article> specification = createSpecification(whereMap);
-		return articleDao.findAll(specification);
-	}
+    /**
+     * 查询全部列表
+     *
+     * @return
+     */
+    public List<Article> findAll() {
+        return articleDao.findAll();
+    }
 
-	/**
-	 * 根据ID查询实体
-	 * @param id
-	 * @return
-	 */
-	public Article findById(String id) {
-		return articleDao.findById(id).get();
-	}
 
-	/**
-	 * 增加
-	 * @param article
-	 */
-	public void add(Article article) {
-		article.setId( idWorker.nextId()+"" );
-		articleDao.save(article);
-	}
+    /**
+     * 条件查询+分页
+     *
+     * @param whereMap
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<Article> findSearch(Map whereMap, int page, int size) {
+        Specification<Article> specification = createSpecification(whereMap);
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        return articleDao.findAll(specification, pageRequest);
+    }
 
-	/**
-	 * 修改
-	 * @param article
-	 */
-	public void update(Article article) {
-		articleDao.save(article);
-	}
 
-	/**
-	 * 删除
-	 * @param id
-	 */
-	public void deleteById(String id) {
-		articleDao.deleteById(id);
-	}
+    /**
+     * 条件查询
+     *
+     * @param whereMap
+     * @return
+     */
+    public List<Article> findSearch(Map whereMap) {
+        Specification<Article> specification = createSpecification(whereMap);
+        return articleDao.findAll(specification);
+    }
 
-	/**
-	 * 动态条件构建
-	 * @param searchMap
-	 * @return
-	 */
-	private Specification<Article> createSpecification(Map searchMap) {
+    /**
+     * 根据ID查询实体
+     *
+     * @param id
+     * @return
+     */
+    public Article findById(String id) {
+        //从redis中取缓存了的数据
+        Article article = (Article) redisTemplate.opsForValue().get("article_" + id);
+        //如果上一步没有取出缓存则从数据库中查询,并添加进redis中,设置过期时间为一天;
+        if (null == article) {
+            article = articleDao.findById(id).get();
+            redisTemplate.opsForValue().set("article_"+id,article,1,TimeUnit.DAYS);
+        }
+        return article;
+    }
 
-		return new Specification<Article>() {
+    /**
+     * 增加
+     *
+     * @param article
+     */
+    public void add(Article article) {
+        article.setId(idWorker.nextId() + "");
+        articleDao.save(article);
+    }
 
-			@Override
-			public Predicate toPredicate(Root<Article> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				List<Predicate> predicateList = new ArrayList<Predicate>();
+    /**
+     * 修改
+     *
+     * @param article
+     */
+    public void update(Article article) {
+        //删除redis中的数据,再更新数据库数据;
+        redisTemplate.delete("article_" + article.getId());
+        articleDao.save(article);
+    }
+
+    /**
+     * 删除
+     *
+     * @param id
+     */
+    public void deleteById(String id) {
+        redisTemplate.delete("article_" + id);
+        articleDao.deleteById(id);
+    }
+
+    /**
+     * 动态条件构建
+     *
+     * @param searchMap
+     * @return
+     */
+    private Specification<Article> createSpecification(Map searchMap) {
+
+        return new Specification<Article>() {
+
+            @Override
+            public Predicate toPredicate(Root<Article> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                List<Predicate> predicateList = new ArrayList<Predicate>();
                 // ID
-                if (searchMap.get("id")!=null && !"".equals(searchMap.get("id"))) {
-                	predicateList.add(cb.like(root.get("id").as(String.class), "%"+(String)searchMap.get("id")+"%"));
+                if (searchMap.get("id") != null && !"".equals(searchMap.get("id"))) {
+                    predicateList.add(cb.like(root.get("id").as(String.class), "%" + (String) searchMap.get("id") + "%"));
                 }
                 // 专栏ID
-                if (searchMap.get("columnid")!=null && !"".equals(searchMap.get("columnid"))) {
-                	predicateList.add(cb.like(root.get("columnid").as(String.class), "%"+(String)searchMap.get("columnid")+"%"));
+                if (searchMap.get("columnid") != null && !"".equals(searchMap.get("columnid"))) {
+                    predicateList.add(cb.like(root.get("columnid").as(String.class), "%" + (String) searchMap.get("columnid") + "%"));
                 }
                 // 用户ID
-                if (searchMap.get("userid")!=null && !"".equals(searchMap.get("userid"))) {
-                	predicateList.add(cb.like(root.get("userid").as(String.class), "%"+(String)searchMap.get("userid")+"%"));
+                if (searchMap.get("userid") != null && !"".equals(searchMap.get("userid"))) {
+                    predicateList.add(cb.like(root.get("userid").as(String.class), "%" + (String) searchMap.get("userid") + "%"));
                 }
                 // 标题
-                if (searchMap.get("title")!=null && !"".equals(searchMap.get("title"))) {
-                	predicateList.add(cb.like(root.get("title").as(String.class), "%"+(String)searchMap.get("title")+"%"));
+                if (searchMap.get("title") != null && !"".equals(searchMap.get("title"))) {
+                    predicateList.add(cb.like(root.get("title").as(String.class), "%" + (String) searchMap.get("title") + "%"));
                 }
                 // 文章正文
-                if (searchMap.get("content")!=null && !"".equals(searchMap.get("content"))) {
-                	predicateList.add(cb.like(root.get("content").as(String.class), "%"+(String)searchMap.get("content")+"%"));
+                if (searchMap.get("content") != null && !"".equals(searchMap.get("content"))) {
+                    predicateList.add(cb.like(root.get("content").as(String.class), "%" + (String) searchMap.get("content") + "%"));
                 }
                 // 文章封面
-                if (searchMap.get("image")!=null && !"".equals(searchMap.get("image"))) {
-                	predicateList.add(cb.like(root.get("image").as(String.class), "%"+(String)searchMap.get("image")+"%"));
+                if (searchMap.get("image") != null && !"".equals(searchMap.get("image"))) {
+                    predicateList.add(cb.like(root.get("image").as(String.class), "%" + (String) searchMap.get("image") + "%"));
                 }
                 // 是否公开
-                if (searchMap.get("ispublic")!=null && !"".equals(searchMap.get("ispublic"))) {
-                	predicateList.add(cb.like(root.get("ispublic").as(String.class), "%"+(String)searchMap.get("ispublic")+"%"));
+                if (searchMap.get("ispublic") != null && !"".equals(searchMap.get("ispublic"))) {
+                    predicateList.add(cb.like(root.get("ispublic").as(String.class), "%" + (String) searchMap.get("ispublic") + "%"));
                 }
                 // 是否置顶
-                if (searchMap.get("istop")!=null && !"".equals(searchMap.get("istop"))) {
-                	predicateList.add(cb.like(root.get("istop").as(String.class), "%"+(String)searchMap.get("istop")+"%"));
+                if (searchMap.get("istop") != null && !"".equals(searchMap.get("istop"))) {
+                    predicateList.add(cb.like(root.get("istop").as(String.class), "%" + (String) searchMap.get("istop") + "%"));
                 }
                 // 审核状态
-                if (searchMap.get("state")!=null && !"".equals(searchMap.get("state"))) {
-                	predicateList.add(cb.like(root.get("state").as(String.class), "%"+(String)searchMap.get("state")+"%"));
+                if (searchMap.get("state") != null && !"".equals(searchMap.get("state"))) {
+                    predicateList.add(cb.like(root.get("state").as(String.class), "%" + (String) searchMap.get("state") + "%"));
                 }
                 // 所属频道
-                if (searchMap.get("channelid")!=null && !"".equals(searchMap.get("channelid"))) {
-                	predicateList.add(cb.like(root.get("channelid").as(String.class), "%"+(String)searchMap.get("channelid")+"%"));
+                if (searchMap.get("channelid") != null && !"".equals(searchMap.get("channelid"))) {
+                    predicateList.add(cb.like(root.get("channelid").as(String.class), "%" + (String) searchMap.get("channelid") + "%"));
                 }
                 // URL
-                if (searchMap.get("url")!=null && !"".equals(searchMap.get("url"))) {
-                	predicateList.add(cb.like(root.get("url").as(String.class), "%"+(String)searchMap.get("url")+"%"));
+                if (searchMap.get("url") != null && !"".equals(searchMap.get("url"))) {
+                    predicateList.add(cb.like(root.get("url").as(String.class), "%" + (String) searchMap.get("url") + "%"));
                 }
                 // 类型
-                if (searchMap.get("type")!=null && !"".equals(searchMap.get("type"))) {
-                	predicateList.add(cb.like(root.get("type").as(String.class), "%"+(String)searchMap.get("type")+"%"));
+                if (searchMap.get("type") != null && !"".equals(searchMap.get("type"))) {
+                    predicateList.add(cb.like(root.get("type").as(String.class), "%" + (String) searchMap.get("type") + "%"));
                 }
-				
-				return cb.and( predicateList.toArray(new Predicate[predicateList.size()]));
 
-			}
-		};
+                return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
 
-	}
+            }
+        };
+
+    }
 
 }
